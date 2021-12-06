c95e2092774f659f367bcf2d7fe699b0e1194e12
cph-jm390 <cph-jm390@cphbusiness.dk>
2021-12-06 10:21:08 +0100

    chancen

--- ICE10_5/src/Chance.java
+++ ICE10_5/src/Chance.java
@@ -1,4 +1,18 @@
 public class Chance {
-    int chanceNum;
-    //TODO math.random
+    private int chanceNum;
+
+
+    public Chance() {
+    }
+
+    public int chanceRoll(){
+        chanceNum = (int) (6*Math.random() + 1);
+        return chanceNum
+
+    }
+
+
 }
+    //TODO math.random
+
+
