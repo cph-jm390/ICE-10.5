629a6a5832b7acbc58b4c6eac1344e3a39ee2e97
cph-jm390 <cph-jm390@cphbusiness.dk>
2021-12-06 10:33:04 +0100

    test test

--- ICE10_5/src/Chance.java
+++ ICE10_5/src/Chance.java
@@ -1,18 +1,22 @@
 public class Chance {
     private int chanceNum;
+    private int chanceNum2;
 
-
-    public Chance() {
+    public int Chance() {
     }
 
     public int chanceRoll(){
         chanceNum = (int) (6*Math.random() + 1);
-        return chanceNum
+        return chanceNum;
 
     }
 
+    public int pickChanceCard(){
+        chanceNum = (int) (50*Math.random() + 1);
+        return chanceNum;
+    }
 
+    
 }
-    //TODO math.random
 
 
