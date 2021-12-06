c245ed9e7355fc3865d5cba654435b2d610b7857
cph-jm390 <cph-jm390@cphbusiness.dk>
2021-12-06 13:24:11 +0100

    upåp

--- ICE10_5/src/Main.java
+++ ICE10_5/src/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Scoreboard scoreboard =new Scoreboard(points,players);
         ui.selectPlayerAmount();
         ui.createPlayer();
 
