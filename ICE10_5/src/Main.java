2f643e041d2dee2412f49a16f0363238b9028030
cph-jm390 <cph-jm390@cphbusiness.dk>
2021-12-06 12:22:10 +0100

    push lol

--- ICE10_5/src/Main.java
+++ ICE10_5/src/Main.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 public class Main {
-    static ArrayList<Player> players = new ArrayList();
+    static ArrayList<Player> players = new ArrayList<>();
+    static ArrayList<Integer> points=new ArrayList<>();
 
     public static UI ui= new UI();
     static int MAX = 8;
@@ -9,8 +10,13 @@ public class Main {
 
 
     public static void main(String[] args) {
+        Scoreboard scoreboard =new Scoreboard(points,players);
         ui.selectPlayerAmount();
         ui.createPlayer();
+        
 
     }
+    }
 }
+
+//TODO for each loop
--- ICE10_5/src/Scoreboard.java
+++ ICE10_5/src/Scoreboard.java
@@ -1,6 +1,30 @@
 import java.util.ArrayList;
 
 public class Scoreboard {
-    ArrayList <Player> players;
-    ArrayList <int> points;
+    //TODO lav kommando !points eller !score for at få visuelt display op for player points
+
+    private ArrayList <Integer> points=new ArrayList<>();
+    private ArrayList <Player> players=new ArrayList<>();
+
+    public ArrayList<Integer> getPoints() {
+        return points;
+    }
+
+    public void setPoints(ArrayList<Integer> points) {
+        this.points = points;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public Scoreboard(ArrayList<Integer> points, ArrayList <Player> players) {
+        this.points = points;
+        this.players=players;
+
+    }
 }
