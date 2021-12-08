2b4caf7f33efaf209cdbb8253474be6b2932c077
SimonLyng <90315141+SimonLyng@users.noreply.github.com>
2021-12-08 12:23:48 +0100

    q

--- ICE10_5/src/Dealer.java
+++ ICE10_5/src/Dealer.java
@@ -6,17 +6,5 @@ public class Dealer extends Player{
     }
 
     // This automates the dealer's play
-    public void dealerPlay(DeckOfCards deckOfCards){
-        System.out.println();
-        while (hand.calculateTotal() <= 16) {
-            System.out.println("Dealer has " + hand.calculateTotal()+ " and hits");
-            hand.addCard(deck.nextCard());
-            System.out.println("Dealer " + this.getHandString(true, false));
-        }
-        if ( hand.calculateTotal() > 21) {
-            System.out.println("Dealer busts. " + this.getHandString(true, false));
-        } else {
-            System.out.println("Dealer stands. " + this.getHandString(true, false));
-        }
     }
 }
